107ff58fc1b8d9594e902ddbbdf9649ac5e889e6
priyansh54 <patelpiyu468@gmail.com>
2023-12-12 14:35:47 +0530

    add new file

---
+++ rock_paper_scissor.py
@@ -0,0 +1,22 @@
+import random
+
+rsp = ['r', 'p', 's']
+user = input("r for rock p for paper s for scissors: ")
+computer = random.choice(rsp)
+
+
+if user == computer:
+  print("it is a tie")
+
+elif user == "r" and computer == "s":
+  print("you win")
+
+elif user == "p" and computer == "r":
+  print("you win")  
+
+elif user == "s" and computer == "p":
+  print("you win")  
+
+else:
+  print("you lost, computer win")  
+
