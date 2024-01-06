5a471be432027a3319bc3ffa046a36d1aee6057c
priyansh54 <patelpiyu468@gmail.com>
2024-01-06 15:26:57 +0530

    Fix error

--- guess.py
+++ guess.py
@@ -1,4 +1,4 @@
-from mimetypes import guess_all_extensions
+
 import random
 
 
@@ -8,16 +8,16 @@ def computer_guess(x):
     feedback = ''
     while feedback != "c":
         if low != high:
-            guess = random.randint(low, high)
+            Guess = random.randint(low, high)
         else:
-            guess = low
-        feedback = input(f"is {guess} too high (h) if guess low (l) if guess is correct (c)").lower()    
+            Guess = low
+        feedback = input(f"is {Guess} too high (h) if guess low (l) if guess is correct (c)").lower()    
         if feedback == "h":
-            high = guess - 1
+            high = Guess - 1
         elif feedback == "l":
-            low = guess + 1    
+            low = Guess + 1    
 
-    print(f'Yay, congrats. You have guessed the number {guess_all_extensions} correctly!!')
+    print(f'Yay, congrats. You have guessed the number {Guess} correctly!!')
 
 
-computer_guess(10000)
+computer_guess(100)
